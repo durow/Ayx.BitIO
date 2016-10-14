6ac6bdbc957a2d1c889de9cdb07c04bab37282da
durow <durow@163.com>
2016-10-14 08:24:38 +0800

    init project

---
+++ Ayx.BitIO/BitReader.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ayx.BitIO
+{
+    public class BitReader
+    {
+        public readonly StringBuilder BinString;
+        public int Position { get; set; }
+        public int Length
+        {
+            get
+            {
+                return BinString.Length;
+            }
+        }
+        public int Remain
+        {
+            get
+            {
+                return BinString.Length * 8 - Position;
+            }
+        }
+
+        public BitReader(byte[] data)
+        {
+            BinString = new StringBuilder(data.Length * 8);
+            for (int i = 0; i < data.Length; i++)
+            {
+                BinString.Append(ByteToBinString(data[i]));
+            }
+            Position = 0;
+        }
+
+        public byte ReadByte(int offset)
+        {
+            var bin = BinString.ToString(offset, 8);
+            return Convert.ToByte(bin, 2);
+        }
+
+        public byte ReadByte()
+        {
+            var result = ReadByte(Position);
+            Position += 8;
+            return result;
+        }
+
+        public int ReadInt(int offset, int bitLength)
+        {
+            var bin = BinString.ToString(offset, bitLength);
+            return Convert.ToInt32(bin, 2);
+        }
+
+        public int ReadInt(int bitLength)
+        {
+            var result = ReadInt(Position, bitLength);
+            Position += bitLength;
+            return result;
+        }
+
+        public Int16 ReadInt16(int offset, int bitLength)
+        {
+            var bin = BinString.ToString(offset, bitLength);
+            return Convert.ToInt16(bin, 2);
+        }
+
+        public Int16 ReadInt16(int bitLength)
+        {
+            var result = ReadInt16(Position, bitLength);
+            Position += bitLength;
+            return result;
+        }
+
+        public bool ReadBool(int offset)
+        {
+            var result = ReadInt(offset, 1);
+            return offset == 1;
+        }
+
+        public bool ReadBool()
+        {
+            var result = ReadBool(Position);
+            Position++;
+            return result;
+        }
+
+        public string ReadBinString(int offset, int bitLength)
+        {
+            return BinString.ToString(offset, bitLength);
+        }
+
+        public string ReadBinString(int bitLength)
+        {
+            var result = ReadBinString(Position, bitLength);
+            Position += bitLength;
+            return result;
+        }
+
+        public static char[] ByteToBinString(byte b)
+        {
+            var result = new char[8];
+
+            for (int i = 0; i < 8; i++)
+            {
+                var temp = b & 128;
+                result[i] = temp == 0 ? '0' : '1';
+                b = (byte)(b << 1);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Ayx.BitIO/BitWriter.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ayx.BitIO
+{
+    public class BitWriter
+    {
+        public readonly StringBuilder BinString;
+
+        public BitWriter()
+        {
+            BinString = new StringBuilder();
+        }
+
+        public BitWriter(int bitLength)
+        {
+            var add = 8 - bitLength % 8;
+            BinString = new StringBuilder(bitLength + add);
+        }
+
+        public void WriteByte(byte b, int bitLength = 8)
+        {
+            var bin = Convert.ToString(b, 2);
+            AppendBinString(bin, bitLength);
+        }
+
+        public void WriteInt(int i, int bitLength)
+        {
+            var bin = Convert.ToString(i, 2);
+            AppendBinString(bin, bitLength);
+        }
+
+        public void WriteChar7(char c)
+        {
+            var b = Convert.ToByte(c);
+            var bin = Convert.ToString(b, 2);
+            AppendBinString(bin, 7);
+        }
+
+        public byte[] GetBytes()
+        {
+            Check8();
+            var len = BinString.Length / 8;
+            var result = new byte[len];
+
+            for (int i = 0; i < len; i++)
+            {
+                var bits = BinString.ToString(i * 8, 8);
+                result[i] = Convert.ToByte(bits, 2);
+            }
+
+            return result;
+        }
+
+        public string GetBinString()
+        {
+            Check8();
+            return BinString.ToString();
+        }
+
+
+        private void AppendBinString(string bin, int bitLength)
+        {
+            if (bin.Length > bitLength)
+                throw new Exception("len is too short");
+            var add = bitLength - bin.Length;
+            for (int i = 0; i < add; i++)
+            {
+                BinString.Append('0');
+            }
+            BinString.Append(bin);
+        }
+
+        private void Check8()
+        {
+            var add = 8 - BinString.Length % 8;
+            for (int i = 0; i < add; i++)
+            {
+                BinString.Append("0");
+            }
+        }
+    }
+}
